43f605f272386e49fa0c20ab023b5748e599dd80
varokas <varokas@panusuwan.com>
2016-09-10 12:33:22 +0700

    Update install_puppet.ps1

--- powershell/install_puppet.ps1
+++ powershell/install_puppet.ps1
@@ -3,4 +3,4 @@ $setupDir = "C:\setup"
 mkdir -f $setupDir
 wget https://downloads.puppetlabs.com/windows/puppet-agent-x64-latest.msi -OutFile "$setupDir\puppet-agent-x64-latest.msi"
 
-Start-Process -Wait - FilePath "msiexec.exe" -ArgumentList "/qn", "/norestart", "/i", "$setupDir\puppet-agent-x64-latest.msi", "PUPPET_MASTER_SERVER=puppet"
+Start-Process -Wait -FilePath "msiexec.exe" -ArgumentList "/qn", "/norestart", "/i", "$setupDir\puppet-agent-x64-latest.msi", "PUPPET_MASTER_SERVER=puppet"
